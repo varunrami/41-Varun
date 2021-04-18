a3652ac0767f3c8a2cf6b107c961710809988f88
varunrami <57570097+varunrami@users.noreply.github.com>
2021-04-18 20:25:14 +0530

    Add files via upload

--- Graph.py
+++ Graph.py
@@ -7,12 +7,10 @@
 import matplotlib.pyplot as plt
 
 
-# In[12]:
+# In[13]:
 
 
-# x axis values
 x1 = [512,1024,2048,3072,4096]
-# corresponding y axis values
 y1 = [4114,27136,256276,796611,1987031]
 plt.plot(x1, y1, label = "Key Generation")
 
@@ -25,15 +23,11 @@ y3=[706,1986,7923,18893,40909]
 plt.plot(x3, y3, label = "Decryption")
 
 plt.yscale('log')
-# naming the x axis
 plt.xlabel('Key Size')
-# naming the y axis
 plt.ylabel('Time (microseconds)')
 plt.xlim([500,4500])
-# giving a title to my graph
-#plt.title('Time taken for RSA Key Generation')
+
 plt.legend()
-# function to show the plot
 plt.show()
 plt.savefig('graph.jpg')
 
